48d7fa5feb1731283a2a931f955fd085ff13c0b6
Flexify <support@flexify.io>
2023-03-18 12:15:23 +0000

    v2.12.13-SNAPSHOT build 1101

--- src/main/java/io/flexify/apiclient/handler/ApiClient.java
+++ src/main/java/io/flexify/apiclient/handler/ApiClient.java
@@ -74,7 +74,7 @@ public class ApiClient {
     this.dateFormat = new RFC3339DateFormat();
 
     // Set default User-Agent.
-    setUserAgent("Swagger-Codegen/2.12.12-SNAPSHOT/java");
+    setUserAgent("Swagger-Codegen/2.12.13-SNAPSHOT/java");
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
